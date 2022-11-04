cb38e57546e70c8681f45b49a3f682e6173a9335
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2022-11-04 21:00:36 +0100

    ✨ Read fees from CSV files

--- R/read_fees.R
+++ R/read_fees.R
@@ -1,17 +1,37 @@
-read_fees <- function(fees_file)
+read_fees <- function(all_fees_file)
 {
-    wide_fees <- readr::read_delim(fees_file, delim = ";", trim_ws = TRUE)
+    all_wide_fees <- purrr::map(
+        all_fees_files,
+        readr::read_delim,
+        delim = ";", trim_ws = TRUE, col_types = readr::cols()
+    )
+
+    all_long_fees <- purrr::map(all_wide_fees, wide_to_long_fees)
+
+    purrr::reduce(all_long_fees, dplyr::full_join, by = c("Date", "HourOfDay")) |>
+        dplyr::arrange(Date, HourOfDay)
+}
+
+
+wide_to_long_fees <- function(wide_fees)
+{
+    if (!(all(c("StartHour", "EndHour") %in% names(wide_fees))))
+    {
+        wide_fees$StartHour <- 1
+        wide_fees$EndHour <- 24
+    }
 
     nested_fees <- wide_fees |>
-        tidyr::fill(Transport, NetTarif, Fees, Margin, FixedPrice, .direction = "downup") |>
         dplyr::mutate(
             Date = purrr::map2(StartDate, EndDate, seq, by = "1 day"),
-            HourOfDay = purrr::map2(HourStart, HourEnd, seq, by = 1),
-            .keep = "unused"
+            HourOfDay = purrr::map2(StartHour, EndHour, seq, by = 1),
+            .keep = "unused",
+            .before = 1
         )
 
-    nested_fees |>
+    long_fees <- nested_fees |>
         tidyr::unnest(Date) |>
-        tidyr::unnest(HourOfDay) |>
-        dplyr::arrange(Date, HourOfDay)
+        tidyr::unnest(HourOfDay)
+
+    return(long_fees)
 }
